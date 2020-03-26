38fdbc4887551c7e4e23c4ecf064111b2313b5a0
Rodrigo Botelho <tato.rodrigo@gmail.com>
2020-03-25 22:54:17 -0300

    adiciona página ideias

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,6 @@
 function onOff(event) {
   event.preventDefault();
   document.querySelector("#modal").classList.toggle("hide");
+  document.querySelector("body").classList.toggle("hide-scroll");
+  document.querySelector("#modal").classList.toggle("show-scroll");
 }
